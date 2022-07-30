b44dfe81e3a634dc79e3576d2712537523d949d7
increpe <increpe@gmail.com>
2022-07-29 21:25:59 -0500

    Attribute Access Error Fixed

--- Transaction.py
+++ Transaction.py
@@ -5,7 +5,7 @@ class Transaction:
         self.transaction_id = transaction_id
         self.time_stamp = time_stamp
         self.resource_hold = [] # list of resources / items locked by a transaction
-        self.transaction_state = TRANSACTION_STATUS.ACTIVE
+        self.transaction_state = TRANSACTION_STATUS['ACTIVE']
         self.waiting = [] # list of waiting operations when transaction is in BLOCKED state
     
     def hold_resource(self, resource):
@@ -13,3 +13,6 @@ class Transaction:
     
     def add_waiting_operation(self, operation):
         self.waiting.append(operation)
+    
+    def __str__(self) -> str:
+        return f"{self.transaction_id} : [{self.time_stamp}, {self.transaction_state}]"
